b6ff60a2fc869573686a263a85dad9ae2756cbd6
LorenzoMinotti-090 <minottilorenzo@gmail.com>
2026-01-20 16:32:20 +0100

    fix: cors

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import moviesRouter from "./routers/movies.js";
+import cors from "cors";
+
 
 const app = express();
 const port = 3000;
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -11,7 +14,7 @@ app.get("/", (req, res) => {
   res.send("server ok");
 });
 
-app.use("/movies", moviesRouter);
+app.use("/api/movies", moviesRouter);
 
 app.listen(port, () => {
   console.log("Server attivo su http://localhost:" + port);
